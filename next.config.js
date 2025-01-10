9a63ed18764ed0ce41502b5954a632b1759a4bce
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-01-10 08:35:21 -0500

    excess files

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-const isProd = process.env.NODE_ENV === 'production';
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isProd ? '/Website-Examples/' : '',
-  basePath: isProd ? '/Website-Examples' : '',
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-     distDir: 'docs',
-  }
-   
-  module.exports = nextConfig
+    basePath: "/Website-Examples",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
