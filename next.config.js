3d88706653bcde36fba46ec254c8a141e8e26035
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-01-10 08:11:50 -0500

    docs folder

--- next.config.js
+++ next.config.js
@@ -7,7 +7,15 @@ const nextConfig = {
     assetPrefix: isProd ? '/Website-Examples/' : '',
   basePath: isProd ? '/Website-Examples' : '',
     output: 'export',
-
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+     distDir: 'docs',
   }
    
   module.exports = nextConfig
