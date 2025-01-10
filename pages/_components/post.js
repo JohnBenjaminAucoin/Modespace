bb0f6d3e2c2142f919620556f284607542e8be4e
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-01-10 11:23:55 -0500

    post.js

---
+++ pages/_components/post.js
@@ -0,0 +1,16 @@
+export default function Post(props) {
+    return (
+      <div>
+    <div>
+      <h1>MODESPACE</h1>
+      <p>A collection of well designed websites to draw inspiration from.</p>
+    </div>
+    <div id='filters'>
+
+    </div>
+    <div id='results'>
+
+    </div>
+    </div>
+    );
+  }
