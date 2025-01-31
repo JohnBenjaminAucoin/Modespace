43d6941b296b68b030bc1ff7bbb39d03d0ff1b44
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-01-31 07:36:59 -0500

    spacing adjustments

--- _components/Site.js
+++ _components/Site.js
@@ -18,6 +18,7 @@ export default function Site({ site }) {
 
   useEffect(() => {
     const siteCardElements = document.querySelectorAll(".siteCardWrapper")
+    
     VanillaTilt.init(siteCardElements, {
       reverse: false,
       glare: true,
