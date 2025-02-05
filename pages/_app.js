0d12533c3ce3bbf32b3f923b2a77f3e3d3c0bb2e
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-02-05 12:35:11 -0500

    changed tagline to description in og: tags

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,8 @@ export default function App({ Component, pageProps }) {
     <>
     <Head>
       <title>Modespace</title>
-      <meta property="og:title" content="Modespace | Strong websites for design inspiration." />
+      <meta property="og:title" content="Modespace" />
+      <meta property='og:description' content="Strong websites for design inspiration."
       <meta property="og:url" content="https://johnbenjaminaucoin.github.io/Modespace/" />
       <meta property="og:image" content="https://johnbenjaminaucoin.github.io/Modespace/images/preview-img.png" />
 
