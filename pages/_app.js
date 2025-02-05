94382583ccbcb29087aa95e8a13acb28e99af3f0
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-02-05 12:35:48 -0500

    missing closing bracket

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
     <Head>
       <title>Modespace</title>
       <meta property="og:title" content="Modespace" />
-      <meta property='og:description' content="Strong websites for design inspiration."
+      <meta property='og:description' content="Strong websites for design inspiration." />
       <meta property="og:url" content="https://johnbenjaminaucoin.github.io/Modespace/" />
       <meta property="og:image" content="https://johnbenjaminaucoin.github.io/Modespace/images/preview-img.png" />
 
