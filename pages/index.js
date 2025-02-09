213a2bb4727c2a25cf88c7522ca8e51c61ccc2b8
Benjamin Aucoin <benjamin.aucoin1@gmail.com>
2025-02-09 12:28:43 -0500

    Added Framer-motion, changed spacing of cards

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 
-import { getSites, getTags } from '../lib/sites'
+import { getSites, getTags } from '../lib/sites';
+import { motion } from 'framer-motion';
 import Site from '../_components/Site';
 import FilterControl from '../_components/FilterControl';
-import React, {createContext, useState} from "react";
+import React, { createContext, useState } from "react";
 export const SelectedTagsContext = createContext(null);
 
-function compareSites(a, b){
+function compareSites(a, b) {
   if (a.title < b.title) {
     return -1;
   } else if (a.title > b.title) {
@@ -24,23 +25,33 @@ export default function Index({ sites, tags }) {
   }
 
   return (
-    <div className="pageContainer">
-    
-      <div className="headingContainer">
-        <h1 className='title'>MODESPACE</h1>
-        <p className='subtitle'>A collection of strong websites for design inspiration.</p>
+    <motion.div initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.3 }}
+    >
+
+      <div className="pageContainer">
+
+        <div className="headingContainer">
+          <h1 className='title'>MODESPACE</h1>
+          <p className='subtitle'>A collection of strong websites for design inspiration.</p>
+        </div>
+        <SelectedTagsContext.Provider value={{ selectedTags, setTags }}>
+          <div id='filters'>
+            <FilterControl tags={tags} />
+          </div>
+          <div className='sitesContainer'
+          >
+            {sites.sort(compareSites).map((site) => (
+              <Site key={site.title} site={site} />
+            ))}
+          </div>
+        </SelectedTagsContext.Provider>
       </div>
-      <SelectedTagsContext.Provider value={{ selectedTags, setTags}}>
-      <div id='filters'>
-        <FilterControl tags={tags} />
-      </div>
-      <div className='sitesContainer'>
-        {sites.sort(compareSites).map((site) => (
-          <Site key={site.title} site={site}/>
-        ))}
-      </div>
-      </SelectedTagsContext.Provider>
-    </div>
+      
+    </motion.div>
+
   );
 }
 
